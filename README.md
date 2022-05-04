5fabcf6518cad978decbfbf3f5c167f5af828d87
Howard Lau <howardlau1999@hotmail.com>
2022-05-04 17:31:32 +0800

    update

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Some micro-benchmarks.
 
 Run `cargo bench` to see the results.
 
-Div: Compare integer division and floating point division.
+Div: Compare integer division and floating point division. (Floating point division is faster!)
 
 SIMD Sum: Sum an array of numbers using SIMD.
--- src/lib.rs
+++ src/lib.rs
@@ -137,12 +137,20 @@ fn test_avx_simd_256() {
 
 pub fn div_u64() {
     black_box(black_box(64 as u64) / black_box(3 as u64));
-    black_box(black_box(64 as f64) / black_box(3 as f64));
-    black_box(black_box(64 as f64) / black_box(3 as f64));
-    black_box(black_box(64 as f64) / black_box(3 as f64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
+    black_box(black_box(64 as u64) / black_box(3 as u64));
 }
 
 pub fn div_f64() {
+    black_box(black_box(64 as f64) / black_box(3 as f64));
+    black_box(black_box(64 as f64) / black_box(3 as f64));
+    black_box(black_box(64 as f64) / black_box(3 as f64));
+    black_box(black_box(64 as f64) / black_box(3 as f64));
     black_box(black_box(64 as f64) / black_box(3 as f64));
     black_box(black_box(64 as f64) / black_box(3 as f64));
     black_box(black_box(64 as f64) / black_box(3 as f64));
